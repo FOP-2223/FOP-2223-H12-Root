2504ea538ffdcdc4f5883d72c80a52867e471337
LukasKlenner <klenner-lukas@t-online.de>
2022-10-06 00:32:16 +0200

    Replace submitter plugin with jagr-gradle snapshot

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,41 +1,33 @@
-import org.sourcegrade.submitter.submit
-
 plugins {
     java
     application
     id("org.sourcegrade.style") version "1.3.0"
-    id("org.sourcegrade.submitter") version "0.4.0"
+    id("org.sourcegrade.jagr-gradle") version "0.6.0-SNAPSHOT"
 }
 
 version = "0.1.0-SNAPSHOT"
 
-repositories {
-    mavenCentral()
-    maven("https://s01.oss.sonatype.org/content/repositories/snapshots/")
-}
-
-submit {
-    assignmentId = "h12"
-    studentId = "ab12cdef"
-    firstName = "sol_first"
-    lastName = "sol_last"
-    requireTests = false
-}
-
-val grader: SourceSet by sourceSets.creating {
-    val test = sourceSets.test.get()
-    compileClasspath += test.output + test.compileClasspath
-    runtimeClasspath += output + test.runtimeClasspath
+jagr {
+    assignmentId.set("h12")
+    submissions {
+        create("main") {
+            studentId.set("ab12cdef")
+            firstName.set("sol_first")
+            lastName.set("sol_last")
+        }
+    }
+    graders {
+        create("grader") {
+            graderName.set("FOP-2223-H12")
+            rubricProviderName.set("h12.H12_RubricProvider")
+        }
+    }
 }
 
 dependencies {
+    implementation("org.tudalgo:algoutils-tutor:0.2.0-SNAPSHOT")
     implementation("org.jetbrains:annotations:23.0.0")
-    "graderCompileOnly"("org.sourcegrade:jagr-launcher:0.4.0") {
-        exclude("org.jetbrains", "annotations")
-    }
-    testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
-    "graderImplementation"("org.tudalgo:algoutils-tutor:0.2.0-SNAPSHOT")
-    implementation("org.tudalgo:algoutils-student:0.2.0-SNAPSHOT")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.9.0")
 }
 
 application {
@@ -57,62 +49,11 @@ tasks {
         workingDir = runDir
         useJUnitPlatform()
     }
-    val graderTest by creating(Test::class) {
-        group = "verification"
-        doFirst {
-            runDir.mkdirs()
-        }
-        workingDir = runDir
-        testClassesDirs = grader.output.classesDirs
-        classpath = grader.compileClasspath + grader.runtimeClasspath
-        useJUnitPlatform()
-    }
-    named("check") {
-        dependsOn(graderTest)
-    }
-    val graderJar by creating(Jar::class) {
-        group = "build"
-        afterEvaluate {
-            archiveFileName.set("FOP-2223-H12-Root-${project.version}.jar")
-            from(sourceSets.main.get().allSource)
-            from(sourceSets.test.get().allSource)
-            from(grader.allSource)
-        }
-    }
-    val graderLibs by creating(Jar::class) {
-        group = "build"
-        duplicatesStrategy = DuplicatesStrategy.EXCLUDE
 
-        // don't include Jagr's runtime dependencies
-        val jagrRuntime = configurations["graderCompileClasspath"]
-            .resolvedConfiguration
-            .firstLevelModuleDependencies
-            .first { it.moduleGroup == "org.sourcegrade" && it.moduleName == "jagr-launcher" }
-            .allModuleArtifacts
-            .map { it.file }
 
-        val runtimeDeps = grader.runtimeClasspath.mapNotNull {
-            if (it.path.toLowerCase().contains("h12") || jagrRuntime.contains(it)) {
-                null
-            } else if (it.isDirectory) {
-                it
-            } else {
-                zipTree(it)
-            }
-        }
-        from(runtimeDeps)
-        archiveFileName.set("FOP-2223-H12-Root-${project.version}-libs.jar")
-    }
-    create("graderAll") {
-        group = "build"
-        dependsOn(graderJar, graderLibs)
-    }
     withType<JavaCompile> {
         options.encoding = "UTF-8"
         sourceCompatibility = "17"
         targetCompatibility = "17"
-    }
-    jar {
-        enabled = false
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,17 @@
 rootProject.name = "FOP-2223-H12-Root-Root"
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        maven("https://s01.oss.sonatype.org/content/repositories/snapshots")
+        mavenCentral()
+    }
+}
+
+pluginManagement {
+    repositories {
+        maven("https://s01.oss.sonatype.org/content/repositories/snapshots")
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
